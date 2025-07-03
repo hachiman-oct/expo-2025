29b73d33f99ce5cfd91c013d002f919d861321aa
hachiman-oct <176100494+hachiman-oct@users.noreply.github.com>
2025-07-03 22:19:57 +0900

    update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,7 @@ self.addEventListener('install', (event) => {
         caches.open('expo-2025-cache').then((cache) => {
             return cache.addAll([
                 '/expo-2025',
+                '/expo-2025/index.html',
                 '/expo-2025/manifest.json',
                 '/expo-2025/favicon.ico',
                 '/expo-2025/icons/icon-192.png',
@@ -10,8 +11,11 @@ self.addEventListener('install', (event) => {
                 '/expo-2025/icons/icon-180.png',
                 '/expo-2025/link-updater.js',
                 '/expo-2025/prep',
+                '/expo-2025/prep/index.html',
                 '/expo-2025/prep/apps',
+                '/expo-2025/prep/apps/index.html',
                 '/expo-2025/pavilions',
+                '/expo-2025/pavilions/index.html',
                 '/expo-2025/logos/logo-66.png',
             ]);
         })
