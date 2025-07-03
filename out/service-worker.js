475eee0156bc22cba38583c1aab9cf435910d9e5
hachiman-oct <176100494+hachiman-oct@users.noreply.github.com>
2025-07-03 22:35:43 +0900

    update css

--- out/service-worker.js
+++ out/service-worker.js
@@ -2,18 +2,21 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('expo-2025-cache').then((cache) => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/favicon.ico',
-                '/icons/icon-192.png',
-                '/icons/icon-512.png',
-                '/icons/icon-180.png',
-                '/link-updater.js',
-                '/prep',
-                '/prep/apps',
-                '/pavilions',
-                '/logos/logo-66.png',
+                '/expo-2025',
+                '/expo-2025/index.html',
+                '/expo-2025/manifest.json',
+                '/expo-2025/favicon.ico',
+                '/expo-2025/icons/icon-192.png',
+                '/expo-2025/icons/icon-512.png',
+                '/expo-2025/icons/icon-180.png',
+                '/expo-2025/link-updater.js',
+                '/expo-2025/prep',
+                '/expo-2025/prep/index.html',
+                '/expo-2025/prep/apps',
+                '/expo-2025/prep/apps/index.html',
+                '/expo-2025/pavilions',
+                '/expo-2025/pavilions/index.html',
+                '/expo-2025/logos/logo-66.png',
             ]);
         })
     );
